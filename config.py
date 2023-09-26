71317f830df7bc4d6160f19c6443864748594766
active <66892517+activ33@users.noreply.github.com>
2023-09-26 17:26:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config(object):
     API_HASH = "7b1b63a5c5a2e53233a0e78727c068d3"
     BOT_TOKEN = "6344846447:AAGw1C34OXF8YLp80newmf9chnUnQuxnTtE"
     OWNER_ID = 689061386
-    AS_ZIP = True
+    AS_ZIP = False
